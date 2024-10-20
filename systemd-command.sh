1d5ea58193db9350a8f14e0b9bd2b8b9006e0d75
Ali Sawari <ali.dragon.swr@gmail.com>
2024-10-20 21:17:55 +0300

    chore: changed files and added more doc

--- systemd-command.sh
+++ systemd-command.sh
@@ -4,5 +4,5 @@ port="3113"
 dns="1.1.1.1"
 secret=""
 
-/root/wstunnel/wstunnel server --dns-resolver dns://$dns --restrict-http-upgrade-path-prefix $secret ws://0.0.0.0:$port
+/root/wstunnel/wstunnel-linux server --dns-resolver dns://$dns --restrict-http-upgrade-path-prefix $secret ws://0.0.0.0:$port
 
