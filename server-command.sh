1667a72f227a51b9a7e908928a34a53b76c6ec0e
Ali Sawari <ali.dragon.swr@gmail.com>
2024-10-20 21:22:12 +0300

    fix: typowq

--- server-command.sh
+++ server-command.sh
@@ -3,5 +3,5 @@
 port="2112"
 dns="1.1.1.1"
 
-./wstunnel server --dns-resolver dns://$dns --restrict-http-upgrade-path-prefix $(cat secret.txt)  ws://0.0.0.0:$port
+./wstunnel-linux server --dns-resolver dns://$dns --restrict-http-upgrade-path-prefix $(cat secret.txt)  ws://0.0.0.0:$port
 
